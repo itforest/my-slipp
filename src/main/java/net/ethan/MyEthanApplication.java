3e84d1e02282fd1ddc729e90dbad9c615cbc5462
itforest <hokyoun88>
2018-01-10 00:07:25 +0900

    initial commit

---
+++ src/main/java/net/ethan/MyEthanApplication.java
@@ -0,0 +1,12 @@
+package net.ethan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEthanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEthanApplication.class, args);
+	}
+}
